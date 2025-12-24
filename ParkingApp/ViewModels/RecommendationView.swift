23c24adddb40f88c1f4ca6df20736c3d472d9e37
luo113-311 <fyluo0926@gmail.com>
2025-12-24 13:50:52 +0800

    修改了停車場列表主頁。 增加了每個停車場列表項（ParkingLotRow）變得更豐富、更專業。 添加了頂部立刻出現推薦分數卡片。 以及頁面ui輕微改動

---
+++ ParkingApp/ViewModels/RecommendationView.swift
@@ -0,0 +1,8 @@
+//
+//  RecommendationView.swift
+//  ParkingApp
+//
+//  Created by User on 24/12/2025.
+//
+
+import Foundation
---
+++ ParkingApp/Views/FilterAndSortView.swift
@@ -0,0 +1,8 @@
+//
+//  FilterAndSortView.swift
+//  ParkingApp
+//
+//  Created by User on 24/12/2025.
+//
+
+import Foundation
